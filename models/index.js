b2b6a1ef2b27c7a4b8b3918dfa51e1cefe0ed7aa
Tien Nguyen <tien.ng1020@gmail.com>
2020-03-22 01:27:19 -0500

    updated

---
+++ models/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    lifechathistory: require("./lifechathistory"),
+    Patient: require("./Patient"),  
+    Book: require("./book"),
+   
+    emergency: require("./emergency"),
+   
+    profile: require("./profile")
+   
+  };
+  
